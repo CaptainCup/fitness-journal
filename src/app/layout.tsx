8d44f3a7c8c8be361416d0290ebfdba5bab11c2f
CaptainCup <poi.lincoln@gmail.com>
2023-07-26 02:44:47 +0800

    pwa added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,6 +11,59 @@ const font = localFont({ src: './DrukTextWide-Medium.woff' })
 export const metadata = {
   title: 'Fitness Journal',
   description: 'An application for tracking the results of your workouts',
+  manifest: '/manifest.json',
+  themeColor: '#A3E635',
+  viewport: {
+    width: 'device-width',
+    initialScale: 1,
+    maximumScale: 1,
+  },
+  icons: {
+    icon: [
+      {
+        url: '/images/icons/icon-192x192.png',
+        sizes: '192x192',
+        type: 'image/png',
+      },
+      {
+        url: '/images/icons/icon-256x256.png',
+        sizes: '256x256',
+        type: 'image/png',
+      },
+      {
+        url: '/images/icons/icon-384x384.png',
+        sizes: '384x384',
+        type: 'image/png',
+      },
+      {
+        url: '/images/icons/icon-512x512.png',
+        sizes: '512x512',
+        type: 'image/png',
+      },
+    ],
+    apple: [
+      {
+        url: '/images/icons/icon-192x192.png',
+        sizes: '192x192',
+        type: 'image/png',
+      },
+      {
+        url: '/images/icons/icon-256x256.png',
+        sizes: '256x256',
+        type: 'image/png',
+      },
+      {
+        url: '/images/icons/icon-384x384.png',
+        sizes: '384x384',
+        type: 'image/png',
+      },
+      {
+        url: '/images/icons/icon-512x512.png',
+        sizes: '512x512',
+        type: 'image/png',
+      },
+    ],
+  },
 }
 
 type RootLayout = {
